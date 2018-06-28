3376e4662bef8450318a2353ae5513963f4f3032
Guenter Roeck <linux@roeck-us.net>
2018-06-28 14:34:03 -0700

    Add ability to specify shutdown command with RUNTEST
    On some systems, reboot works, but poweroff doesn't.
    On other systems, poweroff works, but reboot doesn't.
    Make the necessary command configurable.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- package/busybox/run.sh
+++ package/busybox/run.sh
@@ -4,11 +4,14 @@ cd /
 
 echo "Boot successful."
 
+MESSAGE="Rebooting"	# REBOOT_MESSAGE
+COMMAND="reboot"	# REBOOT_COMMAND
+
 grep "noreboot" /proc/cmdline >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "Rebooting."
-	reboot
+	echo "${MESSAGE}"
+	${COMMAND}
 	sleep 2
 fi
 
