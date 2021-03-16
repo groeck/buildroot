eaea705d938be62b768f013944795ba7fd1eb5c9
Guenter Roeck <linux@roeck-us.net>
2021-03-15 23:02:29 -0700

    buildall, buildone, copyall: Scripts to build and copy all test images
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ buildone.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+IMAGEDIR="images-$(git describe | cut -f1 -d-)"
+
+buildone()
+{
+    local config="$1"
+    local name="${config##kerneltests_}"
+    name="${name%%_defconfig}"
+
+    if [[ ! -e "configs/${config}" ]]; then
+	echo "Configuration ${config} does not exist"
+	return 1
+    fi
+
+    mkdir -p "${IMAGEDIR}"
+    echo "Building ${name}"
+
+    rm -rf output
+
+    make ${config}
+    if make -j "$(nproc)"; then
+	rm -rf ""${IMAGEDIR}"/${name}"
+	mv output/images ""${IMAGEDIR}"/${name}"
+	if [[ -e "${IMAGEDIR}"/${name}/rootfs.btrfs ]]; then
+	    gzip -f "${IMAGEDIR}"/${name}/rootfs.btrfs
+	fi
+	return 0
+    fi
+    return 1
+}
+
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+
+if [[ ${sourced} -eq 0 ]]; then
+    for target in $*; do
+	if [[ ! -e "configs/kerneltests_${target}_defconfig" ]]; then
+	    echo "Configuration file for ${target} does not exist"
+	    exit 1
+	fi
+	if ! buildone "kerneltests_${target}_defconfig"; then
+	    echo "Failed to build ${target}"
+	fi
+    done
+fi
