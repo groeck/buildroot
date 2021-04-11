69c951e57262d9b1f74a1285889265b8a5e3de17
Guenter Roeck <linux@roeck-us.net>
2021-04-11 08:51:42 -0700

    busybox: run.sh: Improve Ethernet tests
    Wait for a few seconds for the Ethernet interface
    to get its IP address, but only if the interface,
    but only if the interface exists.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- package/busybox/run.sh
+++ package/busybox/run.sh
@@ -5,8 +5,24 @@ cd /
 cat /proc/version
 
 net_test_successful=0
-ifconfig eth0 2>/dev/null | grep -q "inet addr:10.0.2.15"
-if [ $? -eq 0 ]; then
+ip_test_successful=0
+
+# wait for a few seconds until the Ethernet interface is up,
+# but only if it exists
+if [ -e /sys/class/net/eth0 ]; then
+    retries=0
+    while [ ${retries} -lt 5 ]; do
+	ifconfig eth0 2>/dev/null | grep -q "inet addr:10.0.2.15"
+	if [ $? -eq 0 ]; then
+	    ip_test_successful=1
+	    break
+	fi
+	retries=$((retries + 1))
+	sleep 1
+    done
+fi
+
+if [ ${ip_test_successful} -ne 0 ]; then
     ping -q -c 1 -s 1000 -W 1 -I eth0 10.0.2.2 >/dev/null
     if [ $? -eq 0 ]; then
 	telnet 10.0.2.2:22 </dev/null >/dev/null 2>dev/null
