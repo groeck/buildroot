16a2acdc8e95c80577e64fa0dc89aa5cff6fe087
Guenter Roeck <linux@roeck-us.net>
2023-06-18 11:48:24 -0700

    run.sh: Run network test as function
    Also simplify code.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- package/busybox/run.sh
+++ package/busybox/run.sh
@@ -4,39 +4,40 @@ cd /
 
 cat /proc/version
 
-net_test_successful=0
-ip_test_successful=0
+network_test()
+{
+    local ip_test_passed=0
 
-# wait for a few seconds until the Ethernet interface is up,
-# but only if it exists
-if [ -e /sys/class/net/eth0 ]; then
-    retries=0
-    while [ ${retries} -lt 5 ]; do
-	ifconfig eth0 2>/dev/null | grep -q "inet addr:10.0.2.15"
-	if [ $? -eq 0 ]; then
-	    ip_test_successful=1
-	    break
-	fi
-	retries=$((retries + 1))
-	sleep 1
-    done
-fi
+    # wait for a few seconds until the Ethernet interface is up,
+    # but only if it exists
+    if [ -e /sys/class/net/eth0 ]; then
+	retries=0
+	while [ ${retries} -lt 5 ]; do
+	    ifconfig eth0 2>/dev/null | grep -q "inet addr:10.0.2.15"
+	    if [ $? -eq 0 ]; then
+		ip_test_passed=1
+		break
+	    fi
+	    retries=$((retries + 1))
+	    sleep 1
+	done
+    fi
 
-if [ ${ip_test_successful} -ne 0 ]; then
-    ping -q -c 1 -s 1000 -W 1 -I eth0 10.0.2.2 >/dev/null
-    if [ $? -eq 0 ]; then
-	telnet 10.0.2.2:22 </dev/null >/dev/null 2>dev/null
+    if [ ${ip_test_passed} -ne 0 ]; then
+	ping -q -c 1 -s 1000 -W 1 -I eth0 10.0.2.2 >/dev/null
 	if [ $? -eq 0 ]; then
-	    net_test_successful=1
+	    telnet 10.0.2.2:22 </dev/null >/dev/null 2>dev/null
+	    if [ $? -eq 0 ]; then
+		echo "Network interface test passed"
+		return
+	    fi
 	fi
     fi
-fi
 
-if [ ${net_test_successful} -ne 0 ]; then
-    echo "Network interface test passed"
-else
     echo "Network interface test failed"
-fi
+}
+
+network_test
 
 echo "Boot successful."
 
